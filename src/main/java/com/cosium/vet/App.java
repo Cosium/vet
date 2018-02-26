2875fbe4bddf4d0133208becbe16cf3d4ce7d89c
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-02-26 22:30:30 +0100

    Refactor App

--- src/main/java/com/cosium/vet/App.java
+++ src/main/java/com/cosium/vet/App.java
@@ -7,6 +7,7 @@ import com.cosium.vet.log.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -16,15 +17,24 @@ public class App {
   private static final List<String> VERBOSE_ARG_NAMES = List.of("-v", "--verbose");
 
   public static void main(String[] args) {
-    boolean stacktrace = Arrays.stream(args).anyMatch(STACKTRACE_ARG_NAMES::contains);
-    boolean verbose = Arrays.stream(args).anyMatch(VERBOSE_ARG_NAMES::contains);
-    LoggerFactory.setPrintStackTrace(stacktrace);
-    LoggerFactory.setPrintContext(verbose);
-    if (verbose) {
-      LoggerFactory.setLevel(Level.TRACE);
-    }
     try {
-      new Vet(true, new DebugOptions(STACKTRACE_ARG_NAMES, VERBOSE_ARG_NAMES)).run(args);
+      boolean stacktrace = Arrays.stream(args).anyMatch(STACKTRACE_ARG_NAMES::contains);
+      boolean verbose = Arrays.stream(args).anyMatch(VERBOSE_ARG_NAMES::contains);
+      if (stacktrace) {
+        LoggerFactory.setPrintStackTrace(true);
+      }
+      if (verbose) {
+        LoggerFactory.setPrintContext(true);
+        LoggerFactory.setLevel(Level.TRACE);
+      }
+      String[] filteredArgs =
+          Arrays.stream(args)
+              .filter(arg -> !STACKTRACE_ARG_NAMES.contains(arg))
+              .filter(arg -> !VERBOSE_ARG_NAMES.contains(arg))
+              .collect(Collectors.toList())
+              .toArray(new String[] {});
+
+      new Vet(true, new DebugOptions(STACKTRACE_ARG_NAMES, VERBOSE_ARG_NAMES)).run(filteredArgs);
     } catch (Exception e) {
       LOG.error(e.getMessage(), e);
       System.exit(1);
