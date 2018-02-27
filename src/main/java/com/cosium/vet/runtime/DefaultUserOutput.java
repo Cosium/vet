30ee8b9e58711fa82aef3e84799eb34a4145367f
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-02-27 04:36:07 +0100

    Add a space after message question

--- src/main/java/com/cosium/vet/runtime/DefaultUserOutput.java
+++ src/main/java/com/cosium/vet/runtime/DefaultUserOutput.java
@@ -1,5 +1,7 @@
 package com.cosium.vet.runtime;
 
+import com.cosium.vet.thirdparty.apache_commons_lang3.StringUtils;
+
 /**
  * Created on 23/02/18.
  *
@@ -8,6 +10,6 @@ package com.cosium.vet.runtime;
 class DefaultUserOutput implements UserOutput {
   @Override
   public void display(String message) {
-    System.out.print(message);
+    System.out.print(message + StringUtils.SPACE);
   }
 }
