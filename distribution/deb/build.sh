47df3a46aafac6658d0c8e51cc9636fa565c79f3
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-03-02 04:35:02 +0100

    Add deb build script

---
+++ distribution/deb/build.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+WORKDIR=$(pwd)
+DIR=vet_$1
+mkdir $DIR
+mkdir $DIR/usr
+mkdir $DIR/usr/local
+mkdir $DIR/usr/local/bin
+mkdir $DIR/usr/local/lib
+mkdir $DIR/usr/local/lib/vet
+wget $2 -O $DIR/usr/local/lib/vet/vet.zip
+unzip $DIR/usr/local/lib/vet/vet.zip -d $DIR/usr/local/lib/vet
+rm $DIR/usr/local/lib/vet/vet.zip
+cd $DIR/usr/local/bin
+ln -sfn ../lib/vet/bin/vet vet
+cd $WORKDIR
+
+mkdir $DIR/DEBIAN
+cat > $DIR/DEBIAN/control <<EOF
+
+Package: vet
+Version: $1
+Section: base
+Priority: optional
+Architecture: amd64
+Maintainer: Réda Housni Alaoui <reda.housnialaoui@cosium.com>
+Description: Gerrit client using pull request review workflow
+
+EOF
+
+dpkg-deb --build $DIR
